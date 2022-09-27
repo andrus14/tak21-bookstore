1224ad70f48fe303b00030d82449844ac48131b8
Nauka <nauka14@gmail.com>
2022-09-27 11:42:56 +0300

    config template and authors

--- book.php
+++ book.php
@@ -8,4 +8,27 @@ $stmt = $pdo->prepare('SELECT * FROM books WHERE id = :id');
 $stmt->execute(['id' => $id]);
 $book = $stmt->fetch();
 
-var_dump($book);
+$stmt = $pdo->prepare('SELECT * FROM authors LEFT JOIN book_authors ON authors.id=book_authors.author_id WHERE book_authors.book_id = :id');
+$stmt->execute(['id' => $id]);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$book['title'];?></title>
+</head>
+<body>
+    <h1><?=$book['title'];?></h1>
+    Autorid: 
+    <?php
+    while ($author = $stmt->fetch())
+    {
+        echo '<li>' . $author['first_name'] . ' ' . $author['last_name'] . '</li>';
+    }
+    ?>
+</body>
+</html>
